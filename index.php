fa84f101c6aba12996d2d9af9e67a51bf2ed2930
Nakmak98 <nakmak1998@gmail.com>
2018-11-18 17:56:54 +0300

    add slim-framework amd slim/Twig to composer.json. Start intergration with Slim

--- index.php
+++ index.php
@@ -1,31 +1,59 @@
 <?php
+//
+//if(isset($_GET['signout'])) {
+//    unset($_COOKIE['user_id']);
+//    unset($_COOKIE['user_role']);
+//    setcookie('user_id', null, -1);
+//    setcookie('user_role' ,null, -1);
+//}
+//
+//if(isset($_GET['signup'])) {
+//    header('Location: account/registration.php');
+//    exit;
+//}
+//
+//if(isset($_GET['login'])) {
+    $template = 2;
+//    include $_SERVER['DOCUMENT_ROOT']."home.php";
+//    exit;
+//}
+//
+//if(isset($_GET['profile'])) {
+    $template = 4;
+//    include $_SERVER['DOCUMENT_ROOT']."home.php";
+//    exit;
+//}
+//
+//$template = 1;
+//include $_SERVER['DOCUMENT_ROOT']."/home.php";
+//
+//
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-if(isset($_GET['signout'])) {
-    unset($_COOKIE['user_id']);
-    unset($_COOKIE['user_role']);
-    setcookie('user_id', null, -1);
-    setcookie('user_role' ,null, -1);
-}
+require 'vendor/autoload.php';
 
-if(isset($_GET['signup'])) {
-    header('Location: account/registration.php');
-    exit;
-}
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
 
-if(isset($_GET['login'])) { 
-    //$template = 2;
-    include $_SERVER['DOCUMENT_ROOT']."home.php";
-    exit;
-}
+$app = new \Slim\App(["settings" => $config]);
 
-if(isset($_GET['profile'])) {
-    //$template = 4;
-    include $_SERVER['DOCUMENT_ROOT']."home.php";
-    exit;
-}
+// Get container
+$container = $app->getContainer();
 
-$template = 1;
-include $_SERVER['DOCUMENT_ROOT']."/home.php";
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('templates', []);
 
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
 
+    return $view;
+};
+
+$app->get('/', function (Request $request, Response $response) {
+    return $this->view->render($response, 'game_page.php');
+});
+$app->run();
 ?>
