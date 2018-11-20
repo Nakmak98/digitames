35a7aed41263ef3c5072cfb923d819f1df25cb24
Nakmak98 <nakmak1998@gmail.com>
2018-11-20 23:15:54 +0300

    fix z-index in cookies

--- dbconnect_anon_user.php
+++ dbconnect_anon_user.php
@@ -1,9 +1,9 @@
 <?php
 function dbconnect() {
     $db = "gamesite";
-    $host = "";
-    $login = "root";
-    $password = "root";
+    $host = "5.23.54.233";
+    $login = "gamesite";
+    $password = "1qaz2wsx3edc";
     $conn = new mysqli($host, $login, $password, $db, 3306);
     
     if ($conn->connect_error) {
