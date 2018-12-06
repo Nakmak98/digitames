5ba8639d93f3e5fe45e29230dfd272fd63096ffb
Nakmak98 <nakmak1998@gmail.com>
2018-12-06 09:59:35 +0300

    remove unusable classes

--- Logic/Locolizer/Locolizer.php
+++ Logic/Locolizer/Locolizer.php
@@ -9,30 +9,15 @@
 namespace Logic\Locolizer;
 
 abstract class Locolizer {
-    static function getInstance($controller){
+    static function getInstance(string $controller){
         static $locolizer;
         switch ($controller){
             case 'HomePage': $locolizer = new HomePageLocolizer();
                 break;
             case 'GamePage': $locolizer = new GamePageLocolizer();
                 break;
-            case 'LoginPage': $locolizer = new LoginLocolizer();
-                break;
-            case 'SignUpPage': $locolizer = new SignUpLocolizer();
-                break;
-            case 'ProfilePage': $locolizer = new ProfileLocolizer();
-                break;
         }
         return $locolizer;
-    }
-
-    static function getBaseContext(){
-        $baseContext['blog'] = gettext("Blog");
-        $baseContext['login'] = gettext("Login");
-        $baseContext['search'] = gettext("Search");
-        $baseContext['lang_pref'] = gettext("Language Preferences");
-        $baseContext['social']= gettext("Social Media");
-        return $baseContext;
     }
 }
 
