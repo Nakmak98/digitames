2a9acb61e18072294cc2574db26d0929f1b949c8
Nakmak98 <nakmak1998@gmail.com>
2019-01-24 00:02:44 +0300

    recode BasicAuth with Manager

--- Logic/BasicAuth.php
+++ Logic/BasicAuth.php
@@ -7,27 +7,28 @@
  */
 
 namespace Logic;
-require_once 'Db/Db.php';
+require_once 'Manager.php';
 
 class BasicAuth extends AuthenticateUser {
 
-    protected $dbconn;
+    protected $manager;
     protected $login;
     protected $password;
     protected $user;
 
     function __construct($params) {
         parent::__construct();
-        $this->dbconn = \Db::getConnection();
+        $this->manager = new Manager();
         $this->login = $params['email'];
         $this->password = $params['password'];
     }
 
     public function authenticate() {
-        $sql = "SELECT id, password FROM users WHERE email = '$this->login'";
-        $results = $this->dbconn->query($sql);
-        $this->user = $results->fetch_assoc();
-        if (($results->num_rows) and (password_verify($this->password, $this->user['password']))) {
+        $sql = "SELECT id, password FROM users WHERE email = ?";
+        $param_arr[] = "s";
+        $param_arr[] = &$this->login;
+        $this->user = $this->manager->getPreparedAssocResult($sql, $param_arr);
+        if (password_verify($this->password, $this->user['password'])) {
             return true;
         }
         return false;
@@ -36,9 +37,11 @@ class BasicAuth extends AuthenticateUser {
     public function login() {
         session_start();
         $sessid = session_id();
-        $id = $this->user['id'];
-        $sql = "UPDATE user_data SET session_id='$sessid' WHERE user_id='$id'";
-        $this->dbconn->query($sql);
+        $sql = "UPDATE user_data SET session_id=? WHERE user_id = ?";
+        $param_arr[] = "si";
+        $param_arr[] = &$sessid;
+        $param_arr[] = &$this->user['id'];
+        $this->manager->getPreparedResult($sql, $param_arr);
         setcookie("sessid", $sessid, time() + (86400 * 30), "/");
     }
 }
